423f4da58f80c8f5e7d86085d5eb2772f9af9b21
Jonathan Yantz <ijryantz@gmail.com>
2018-03-29 21:14:28 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# A RESTful Demo
+
+A demo for how to use the RESTful programming style.
+
+## What it Does
+
+* Covers `GET`, `POST`, `PUT`, `DELETE`.
+    * `GET` : retrieves data.
+    * `POST` : creates data.
+    * 'PUT' : updates data.
+    * 'DELETE': removes data.
+* All of the functionality is provided through `.htaccess` rules.
+
+## Try the Framework
+
+Index Page
+http://git.avidwebco.com/restful-demo/
+
+Customer Data
+http://git.avidwebco.com/restful-demo/customer/12345
+
+Product Data
+http://git.avidwebco.com/restful-demo/product/12345
---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+error_reporting(-1);
+session_start();
+
+$url = (isset($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+$host = (isset($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'];
+$uri = trim($_SERVER['REQUEST_URI']);
+
+if ($uri === '/') {
+    // Do nothing because there is no path.
+    echo 'No data requested.';
+} else {
+    // Split the request to see what data is being requested.
+    $path = explode('/', trim($uri, '/'));
+}
+?>
+
+<html>
+    <head></head>
+
+    <body>
+        <h2>URL/URI Information</h2>
+        <table>
+            <tr>
+                <td>Full URL</td>
+                <td><?php echo $url; ?></td>
+            </tr>
+            <tr>
+                <td>HOST</td>
+                <td><?php echo $host; ?></td>
+            </tr>
+            <tr>
+                <td>URI</td>
+                <td><?php echo $uri; ?></td>
+            </tr>
+        </table>
+
+        <br>
+
+        <?php
+        if ($path[0] == 'customer') {
+            echo '<h2>This page would show the customer with cust. no. ' . $path[1] . ', and all of their information.</h2>';
+        } elseif ($path[0] == 'product') {
+            echo '<h2>This page would show the product with prod. no. ' . $path[1] . ', and all of the info about that product.</h2>';
+        } else {
+            echo '<h2>Error, no information returned.</h2>';
+        }
+        ?>
+    </body>
+</html>
